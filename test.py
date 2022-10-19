7b161adc320e7569588e14de69b2a8b2e08016ee
shubhamDev5 <kilchatwars123@gmail.comgit config user.name shubhamDev5git config user.email kilchatwars123@gmail.com>
2022-10-19 05:35:17 +0100

    Adding python code

---
+++ test.py
@@ -0,0 +1,16 @@
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, Bacon and Spam (4 more servings of Spam)
+
+spam_amount += 4
+
+if spam_amount > 0 :
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+
+print(viking_song)
+
+print(289/10)
+print(289//10)
